e0efd0737b94ad1de5c4b3326aeccab763bf2540
L-Airennn <muhammadmumin082@gmail.com>
2025-05-28 23:55:30 +0700

    f

--- data/products.js
+++ data/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",f
     rating: {
       stars: 4.5,
       count: 87
