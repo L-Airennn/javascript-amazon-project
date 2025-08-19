55f5dc1f51eb0d785eaaf61d25ec0c7632d14cb6
L-Airennn <muhammadmumin082@gmail.com>
2025-08-19 21:15:10 +0700

    Create automated tes for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,29 @@
+import { formatCurrency } from "../script/utils/money.js";
+
+console.log('Test suite : formatCurrency');
+
+console.log('Converts cents into dollars');
+
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('Work with 0');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('Rounds up to the nearest cent');
+
+if (formatCurrency (2000.5) === '20.01') {
+  console.log ('passed');
+} else {
+  console.log('failed');
+}
+
